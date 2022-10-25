d3e302539b3ad27ff88d338eb7f006172afb511b
Ingrid Gama <ingridgamaa96@gmail.com>
2022-10-25 11:21:42 -0300

    incluindo lampada quebrada

--- lampada.js
+++ lampada.js
@@ -3,12 +3,19 @@ const turnOf = document.getElementById('turnOf');
 const lamp = document.getElementById('lamp');
 
 function lampOn(){
-    lamp.src = './img/ligada.jpg'
+    lamp.src = './img/ligada.jpg';
 }
 
 function lampOf(){
-    lamp.src = './img/desligada.jpg'
+    lamp.src = './img/desligada.jpg';
+}
+
+function lampBroken(){
+    lamp.src = './img/quebrada.jpg';    
 }
 
 turnOn.addEventListener('click', lampOn);
-turnOf.addEventListener('click', lampOf)
+turnOf.addEventListener('click', lampOf);
+lamp.addEventListener('mouseover' , lampOn);
+lamp.addEventListener('mouseleave', lampOf);
+lamp.addEventListener('dblclick', lampBroken);
